8d842595323fe22faf7d09a5556683ea66ee9ebe
helen <hhma@alumni.cpp.edu>
2020-10-10 20:54:45 -0400

    add user failover for the user service unavailable

--- user-failover/src/main/java/com/codespacelab/config/UserFailoverHandler.java
+++ user-failover/src/main/java/com/codespacelab/config/UserFailoverHandler.java
@@ -1,19 +1,13 @@
 package com.codespacelab.config;
 
-import com.codespacelab.model.UserDto;
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 import org.springframework.http.MediaType;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Component
 public class UserFailoverHandler {
-
 
     public Mono<ServerResponse> getValidated(ServerRequest request){
          return ServerResponse.ok()
